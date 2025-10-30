010154218361c155ee7f6660aa60cb1de33dd9dc
ravikantk42 <80306841+ravikantk42@users.noreply.github.com>
2025-10-29 22:27:02 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
-terraform {
-  required_providers {
-    aws ={
-        source = "hashicorp/aws"
-        version = ">=6.18.0"
-    }
-  }
-}
+
 resource "aws_instance" "myec2" {
   ami           = "ami-052064a798f08f0d3"
   instance_type = "t3.micro"
